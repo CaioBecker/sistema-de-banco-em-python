0e7f8b8354a50c8c5bb71dae9b99da319e419a18
CaioBecker <83302532+CaioBecker@users.noreply.github.com>
2022-08-17 07:41:53 -0300

    tudo

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
-from classes.cp import ContaPoupanca
+from classes.banco import Banco
 from classes.cc import ContaCorrente
+from classes.cliente import Cliente
+from classes.conta import Conta
+from classes.cp import ContaPoupanca
+from classes.pessoa import Pessoa
 
-cp = ContaPoupanca(111, 10, 0)
+banco = Banco()
 
-cp.depositar(100)
+cliente1 = Cliente('luiz', 30)
 
-cp.sacar(5)
+cliente2 = Cliente('Maria', 18)
 
-print('##########################')
+cliente3 = Cliente('João', 50)        
 
+conta1 = ContaPoupanca(1111, 254136, 0)
 
-cp = ContaCorrente(111, 10, 0)
+conta2 = ContaCorrente(2222, 254137, 0)
 
-cp.depositar(100)
+conta3 = ContaPoupanca(1313, 254138, 0)
 
-cp.sacar(200)
+cliente1.inserir_conta(conta1)
+cliente2.inserir_conta(conta2)
+cliente3.inserir_conta(conta3)
+
+if banco.autenticar(cliente1):
+    cliente1.conta.depositar(40)
+    cliente1.conta.sacar(20)
+else:
+    print("Cliente invalido")
+
